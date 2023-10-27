a2d6be7980025453efa32811ea0640c33317e72e
MatheusSQueiroz <matheussq15@gmail.com>
2023-10-26 21:08:38 -0300

    Criando Validator

---
+++ donet-test/Validator/ProdutoValidator.cs
@@ -0,0 +1,15 @@
+﻿using donet_test.Model;
+using FluentValidation;
+
+namespace donet_test.Validator
+{
+    public class ProdutoValidator : AbstractValidator<Produto>
+    {
+        public ProdutoValidator()
+        {
+            RuleFor(p => p.Nome)
+                .NotEmpty()
+                .MaximumLength(255);
+        }
+    }
+}
