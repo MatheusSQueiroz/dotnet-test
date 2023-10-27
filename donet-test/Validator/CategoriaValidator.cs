98283f8300aed34d3e821c3daeeeacabd8efbfd1
MatheusSQueiroz <matheussq15@gmail.com>
2023-10-26 23:27:52 -0300

    Criando validator

---
+++ donet-test/Validator/CategoriaValidator.cs
@@ -0,0 +1,15 @@
+﻿using donet_test.Model;
+using FluentValidation;
+
+namespace donet_test.Validator
+{
+    public class CategoriaValidator : AbstractValidator<Categoria>
+    {
+        public CategoriaValidator()
+        {
+            RuleFor(c => c.Nome)
+                .NotEmpty()
+                .MaximumLength(255);
+        }
+    }
+}
