bd62b4655368a4344406d74b747a164bac990d3d
MatheusSQueiroz <matheussq15@gmail.com>
2023-10-26 22:14:14 -0300

    Criando Service e Interface

---
+++ donet-test/Service/IProdutoService.cs
@@ -0,0 +1,14 @@
+﻿using donet_test.Model;
+
+namespace donet_test.Service
+{
+    public interface IProdutoService
+    {
+        Task<IEnumerable<Produto>> GetAll();
+        Task<Produto?> GetById(long id);
+        Task<IEnumerable<Produto>> GetByNome(string nome);
+        Task<Produto?> Create(Produto produto);
+        Task<Produto?> Update(Produto produto);
+        Task<Produto?> Delete(long id);
+    }
+}
