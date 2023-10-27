edf71608c087aba5fffeea0442afc8aaa323e2b1
MatheusSQueiroz <matheussq15@gmail.com>
2023-10-26 22:22:33 -0300

    Injeção de dependência e GetAll

---
+++ donet-test/Controller/ProdutoController.cs
@@ -0,0 +1,27 @@
+﻿using donet_test.Model;
+using donet_test.Service;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace donet_test.Controller
+{
+    [Route("~/produtos")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        private readonly IProdutoService _produtoService;
+        private readonly IValidator<Produto> _produtoValidator;
+
+        public ProdutoController(IProdutoService produtoService, IValidator<Produto> produtoValidator)
+        {
+            _produtoService = produtoService;
+            _produtoValidator = produtoValidator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            return Ok(await _produtoService.GetAll());
+        }
+    }
+}
--- donet-test/Service/Implements/ProdutoService.cs
+++ donet-test/Service/Implements/ProdutoService.cs
@@ -1,22 +1,21 @@
-﻿using donet_test.Model;
+﻿using donet_test.Data;
+using donet_test.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace donet_test.Service.Implements
 {
     public class ProdutoService : IProdutoService
     {
-        public Task<Produto?> Create(Produto produto)
+        private readonly AppDbContext _context;
+
+        public ProdutoService(AppDbContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
         }
 
-        public Task<Produto?> Delete(long id)
+        public async Task<IEnumerable<Produto>> GetAll()
         {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<Produto>> GetAll()
-        {
-            throw new NotImplementedException();
+            return await _context.Produtos.ToListAsync();
         }
 
         public Task<Produto?> GetById(long id)
@@ -29,9 +28,20 @@ namespace donet_test.Service.Implements
             throw new NotImplementedException();
         }
 
+            public Task<Produto?> Create(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+
         public Task<Produto?> Update(Produto produto)
         {
             throw new NotImplementedException();
         }
+
+        public Task<Produto?> Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
