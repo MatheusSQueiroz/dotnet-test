f974573f475ebc350910974c2b84865906d3fb8c
MatheusSQueiroz <matheussq15@gmail.com>
2023-10-26 21:04:52 -0300

    Criando Model Produto

---
+++ donet-test/Model/Produto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace donet_test.Model
+{
+    public class Produto
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        [Column(TypeName = "varchar")]
+        [StringLength(50)]
+        public string Nome { get; set; } = string.Empty;
+
+        [Column(TypeName = "decimal(6,2)")]
+        public decimal Preco { get; set; }
+    }
+}
